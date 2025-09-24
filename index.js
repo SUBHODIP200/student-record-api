deee69902c2f3f6c7be3328fa8c03f1893557407
Subhadip Mondal <87157716+SUBHODIP200@users.noreply.github.com>
2025-09-24 14:31:58 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,6 +2,7 @@
 require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
+const cors = require("cors");
 
 const authRoutes = require('./routes/auth');
 const studentRoutes = require('./routes/students');
